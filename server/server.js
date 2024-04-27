fe8bef55d7864cda832d5b672a3cc6e05d5073a0
prs-dev <pratyushsri.25@gmail.com>
2024-04-27 23:18:34 +0530

    deployment settings

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const cookieParser = require("cookie-parser")
+const path = require('path')
 
 const connectDB = require('./connectDB')
 const authRoutes = require('./routes/authRoutes')
@@ -10,6 +11,7 @@ const userRoutes = require('./routes/userRoutes')
 const {app, server} = require("./socket/socket")
 
 const PORT = process.env.PORT || 5000
+const  __dirname2 = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -17,6 +19,12 @@ app.use(cookieParser())
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
 app.use('/api/user', userRoutes)
+
+app.use(express.static(path.join(__dirname2, "/client/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname2, "client", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectDB()
